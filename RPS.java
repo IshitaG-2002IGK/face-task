3a64f29c0fee8a1d8ab625f056378faa1a3f8b34
Ishita <ishitagops12@gmail.com>
2023-01-29 17:53:25 +0530

    code cleanup

--- RPS.java
+++ RPS.java
@@ -1,5 +1,5 @@
+import java.util.*; 
 import java.util.Scanner;
-// import java.util.*; 
 import java.util.HashMap; // Key index operations like updation, deletion etc is easier!
 
 // GAME INDEX //
